04d285154dd43e44f4b9940592ed6dc2bc7e8709
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2024-03-01 08:03:17 -0800

    connected to mongod

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
 const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("mongoDb is connected");
+}).
+catch((err) => {
+    console.log(err);
+})
 
 
 app.listen(3000, () => {
