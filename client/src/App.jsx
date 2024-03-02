0b5546367d3a173fa988b7d131e6abb5433584bf
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2024-03-02 04:03:39 -0800

    worked on footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Route path='/signin' element={<SignIn/>}/>
         <Route path='/signup' element={<SignUp/>}/>
       </Routes>
+        <Footer/>
     </BrowserRouter>
   )
 }
