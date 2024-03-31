abba37b94ef64690ad3e44e1563bfbf724b31d95
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2024-03-31 15:41:30 -0700

    fixed the bug with firebase

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,8 +1,10 @@
-import { Button, TextInput } from 'flowbite-react'
+import { Alert, Button, TextInput } from 'flowbite-react'
 import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import  {getStorage, getDownloadURL, ref,  uploadBytesResumable, } from 'firebase/storage';
 import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function DashProfile() {
   const {currentUser} = useSelector(state=> state.user)
@@ -58,13 +60,33 @@ uploadTask.on(
       <h1 className='my-7 text-center font-semibold text-3xl'> Profile</h1>
       <form className='flex flex-col gap-4'> 
       <input type='file' accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePickerRef.current.click()}>
+        <div className=" relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePickerRef.current.click()}>
+
+          {imageFileUploadProgress && (
+            <CircularProgressbar value={imageFileUploadProgress || 0 } text={`${imageFileUploadProgress}%`} strokeWidth={5} styles={{
+              root: {
+                width: '100%',
+                height: '100%',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+              },
+
+              path: {
+                stroke:  `rgba(62, 153, 199, ${imageFileUploadProgress / 100})`,
+              },
+            }} 
+            />
+
+          )}
           <img
             src={imageFileUrl ||  currentUser.proflePicture}
             alt="user"
-            className='rounded-full w-full h-full border-8 border-[lightgray] object-cover'
+            className={`rounded-full w-full h-full border-8 border-[lightgray] ${imageFileUploadProgress && imageFileUploadProgress < 100 && 'opacity-60'}`}
           />
         </div>
+        {imageFileUploadError && <Alert color='failure'> {imageFileUploadError}</Alert>}
+        
           <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}/>
           <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}/>
           <TextInput type='password' id='password' placeholder='password' />
