8cf816442c9d87e817b9d87e0f8e71897da1606c
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2024-04-01 00:45:19 -0700

    updating user is made possible

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import User from '../models/user.model.js';
 
 export const test = (req, res) => {
     res.json({message: "API is workingggg!"})
@@ -17,8 +18,10 @@ export const updateUser = async (req, res, next) => {
         req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
     if(req.body.username){
-        if(req.body.username.lenght < 7 || req.body.username.length > 20){
-            return next(errorHandler(400, 'Username must be between 7 and 20 characters'));
+        if (req.body.username.length < 7 || req.body.username.length > 20) {
+            return next(
+              errorHandler(400, 'Username must be between 7 and 20 characters')
+            );
         }
         if(req.body.username.includes(' ')){
             return next(errorHandler(400, 'Username must be lowercase'));
