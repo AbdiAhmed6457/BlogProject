2bb8bbe6da0cedce1866819fb11db3a8c897078d
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2024-04-01 00:36:36 -0700

    worked on update

--- api/routers/user.routers.js
+++ api/routers/user.routers.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import {test} from '../controllers/user.controller.js'
+import {
+    // deleteUser,
+    // getUser,
+    // getUsers,
+    // signout,
+    test,
+    updateUser,
+  } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test)
+router.put('/update/:userId',verifyToken, updateUser);
 
 export default router;
