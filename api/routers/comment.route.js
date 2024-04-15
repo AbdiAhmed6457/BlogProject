475e32a642b12acc8ca2114dcfcd20d72819b439
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2024-04-14 23:13:14 -0700

    solved error encountered with comment section

--- api/routers/comment.route.js
+++ api/routers/comment.route.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import {
-  createComment,
   deleteComment,
   editComment,
   getPostComments,
   getcomments,
   likeComment,
 } from '../controllers/comment.controller.js';
+import { createComment } from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
