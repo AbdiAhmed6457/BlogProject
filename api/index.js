7eca7f3b7cf572ae5d402bab14bd4fac8dad7b71
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2024-04-16 08:33:07 -0700

    still dealing with error on render while deploying the web

--- api/index.js
+++ api/index.js
@@ -17,15 +17,17 @@ catch((err) => {
     console.log(err);
 })
 
+
 const __dirname = path.resolve();
 
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-    console.log("listening to port 3000....")
-})
+  console.log('Server is running on port 3000!');
+});
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
@@ -33,16 +35,17 @@ app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
+
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-  });
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message ||  'internal Server Error';
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    })
-})
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
