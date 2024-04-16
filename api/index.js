37238c8587a705b33ef5a8e5b933360f78211e6e
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2024-04-16 09:05:12 -0700

    trying vercel for deployment

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 import postRoutes from './routers/post.route.js'
 import mongoose from 'mongoose';
 import commentRoutes from './routers/comment.route.js'
-import path from 'path';
+import cors from 'cors'
 dotenv.config();
 
 mongoose.connect(process.env.MONGOURL)
@@ -18,9 +18,14 @@ catch((err) => {
 })
 
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 const app = express();
+app.use(cors(
+   { origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true}
+))
 
 app.use(express.json());
 app.use(cookieParser());
@@ -34,11 +39,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
